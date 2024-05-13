a992af386bbd434b83ac25b7ac33d2a406666587
deuhji <rascielvepsg@hotmail.com>
2024-05-13 10:52:46 +0700

    demo task

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-const aliceBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: aliceAddress,
-  minimumLedgerVersion: BigInt(response.version),
-});
-const bobBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: bobAddress,
-});
-const sponsorBalanceAfter = await aptos.getAccountCoinsData({
-  accountAddress: sponsorAddress,
-});
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
